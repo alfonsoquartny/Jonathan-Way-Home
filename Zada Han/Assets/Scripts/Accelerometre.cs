564cab9c722ea7001b9b02c50869cd7c0de749fe
emrecds <seyrelmeyen2@outlook.com>
2023-02-11 03:50:58 +0300

    animation fixes

--- Zada Han/Assets/Scripts/Accelerometre.cs
+++ Zada Han/Assets/Scripts/Accelerometre.cs
@@ -9,18 +9,44 @@ public class Accelerometre : MonoBehaviour
    public float dirX;
   public  float moveSpeed;
 
+    bool right;
+    bool left;
+
+    Animator animator;
+
+
     void Start()
     {
         rb=GetComponent<Rigidbody>();
+     animator=GetComponent<Animator>();
+
     }
 
     void Update()
     {
+        animator.SetFloat("horizontalP",dirX);
      dirX=Input.acceleration.x*moveSpeed;
         transform.position=new Vector2(Mathf.Clamp(transform.position.x,-7.5f,7.5f),transform.position.y);
+
+
+      if (Input.acceleration.x > 0)
+        {
+            Debug.Log("SAÐ"); 
+            right=true;
+
+        }
+
+        if (Input.acceleration.x < 0)
+        {
+            Debug.Log("SOL");
+            left=true;
+        }
+       
+
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(dirX, 0f);
+
     }
 }
