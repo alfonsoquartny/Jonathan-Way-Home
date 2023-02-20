b3d85773a6f2f55b7fe3c0b8a8d1570c7944d538
emrecds <seyrelmeyen2@outlook.com>
2023-02-20 17:30:12 +0300

    fixes

---
+++ Zada Han/Assets/Scripts/StoneMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneMovement : MonoBehaviour
+{
+    public Vector3 vec;
+    public float speed;
+
+
+    public float vecZ;
+    void Start()
+    {
+        vec = gameObject.transform.position;
+        vecZ = gameObject.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        gameObject.transform.position = vec;
+
+        vec = transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, vecZ);
+
+        vecZ -= Time.deltaTime * speed;
+
+    }
+}
